05847362d0632b19baf6d8b1ddb678f210140e3e
Daniyar <daniyardake@gmail.com>
2021-01-14 02:08:57 +0600

    stories added

--- js/stories.js
+++ js/stories.js
@@ -1,26 +1,20 @@
 function storyFilter(control)
 {
-    daniyar = document.getElementById("daniyar-stories");
-    dina = document.getElementById("dina-stories");
+    kids = document.getElementById("kids-stories");
     family = document.getElementById("family-stories");
     friends = document.getElementById("friends-stories");
 
     if (control.value=='all'){
-        daniyar.style.display = 'block';
-        dina.style.display = 'block';
+        kids.style.display = 'block';
         family.style.display = 'block';
         friends.style.display = 'block';
     } else{
-        daniyar.style.display = 'none';
-        dina.style.display = 'none';
+        kids.style.display = 'none';
         family.style.display = 'none';
         friends.style.display = 'none';
 
-        if (control.value == 'daniyar'){
-            daniyar.style.display = 'block';
-        }
-        if (control.value == 'dina'){
-            dina.style.display = 'block';
+        if (control.value == 'kids'){
+            kids.style.display = 'block';
         }
         if (control.value == 'family'){
             family.style.display = 'block';
